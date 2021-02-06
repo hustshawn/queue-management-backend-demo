9de0dcb31a212f9cde7f8d419917df09513b526a
Shawn Zhang <shawn.zhang@lalamove.com>
2021-02-06 23:20:15 +0800

    Use redis to save the counter

--- app.py
+++ app.py
@@ -1,26 +1,51 @@
+import time
+
 from flask import Flask
+import redis
+
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
 
 class Ticket():
     def __init__(self):
-        self.rear = 0
-        self.current = 0
+        cache.set('rear', 0)
+        cache.set('current', 0)
+        self.rear = cache.get('rear').decode('utf-8')
+        self.current = cache.get('current').decode('utf-8')
     
     def get_current(self):
+        current = cache.get('current')
+        if not current:
+            return '0'
+        self.current = current.decode('utf-8')
         return self.current
 
     def get_rear(self):
+        self.rear = cache.get('rear').decode('utf-8')
         return self.rear
 
     def incr_current(self):
-        self.current += 1
+        self.current = cache.incr('current')
         return self.current
     
     def incr_rear(self):
-        self.rear += 1
+        self.rear = cache.incr('rear')
         return self.rear
 
+
 ticket = Ticket()
+
+def incr_key(key):
+    retries = 5
+    while True:
+        try:
+            return cache.incr(key)
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello_world():
@@ -37,8 +62,9 @@ def resolve_ticket():
 def core(action):
     rear = ticket.get_rear()
     current = ticket.get_current()
-    if rear < current:
+    if int(rear) < int(current):
         return  str(current)
+
     if action == 'retrieve':
         result = ticket.incr_rear()
     if action == 'resolve':
